07c3cec6c2315f1e284a35fa817cdb827a2cd212
Lee-folkes <154321496+Lee-folkes@users.noreply.github.com>
2026-02-07 16:46:00 +0000

    Enhance login page layout and styling for improved user experience

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -1,26 +1,28 @@
 @extends('layouts.app')
 @section('content')
-<div class="login-container">
-    <div class="div">
-        
-    </div>
-    <div class="form-container">
-        <div class="login-header">
-            <h1>Login to the Vault</h1>
+
+<div class="container">
+    <div class="login-card">
+        <div class="login-logo">
+            <img src="{{ asset('images/logo.png') }}" alt="Glos Bike Vault logo">
         </div>
         <div class="login-form">
+            <div class="header">
+                <h1>Welcome to the Glos Bike Vault</h1>
+                <p>Please enter your details</p>
+            </div>
             <form id="loginForm">
                 <input type="email" name="email" id="email" placeholder="Email" required>
                 <input type="password" name="password" id="password" placeholder="Password" required>
                 <button type="submit">Login</button>
-            </form>
-            <p>Don't have an account? <a href="/register">Register here</a></p>
+                <div class="register">
+                    <p>Don't have an account?</p>
+                    <button type="button" onclick="window.location.href='/register'">Register</button>
+                </div>
+            </form>        
+            
             <p id="message"></p>
-        </div>
-
-    </div>
-    <div class="logo">
-        <img src="/images/logo.png" alt="Vault Logo">
+        </div> 
     </div>
 </div>
 @endsection
