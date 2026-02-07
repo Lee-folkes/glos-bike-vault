89a6f80842a1fa1b1aa0515eeef753c71501b88f
Lee-folkes <154321496+Lee-folkes@users.noreply.github.com>
2026-02-07 08:09:42 +0000

    Refactor code structure for improved readability and maintainability

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -1,11 +1,26 @@
 @extends('layouts.app')
 @section('content')
-<h1>Login to the Vault</h1>
-    <form id="loginForm">
-        <input type="email" name="email" id="email" placeholder="Email" required>
-        <input type="password" name="password" id="password" placeholder="Password" required>
-        <button type="submit">Login</button>
-    </form>
-    <p>Don't have an account? <a href="/register">Register here</a></p>
-    <p id="message"></p>
+<div class="login-container">
+    <div class="div">
+        
+    </div>
+    <div class="form-container">
+        <div class="login-header">
+            <h1>Login to the Vault</h1>
+        </div>
+        <div class="login-form">
+            <form id="loginForm">
+                <input type="email" name="email" id="email" placeholder="Email" required>
+                <input type="password" name="password" id="password" placeholder="Password" required>
+                <button type="submit">Login</button>
+            </form>
+            <p>Don't have an account? <a href="/register">Register here</a></p>
+            <p id="message"></p>
+        </div>
+
+    </div>
+    <div class="logo">
+        <img src="/images/logo.png" alt="Vault Logo">
+    </div>
+</div>
 @endsection
