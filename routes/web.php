c23744062ab88e4844130bee98201082cc79bac3
Lee-folkes <154321496+Lee-folkes@users.noreply.github.com>
2026-02-20 12:43:37 +0000

    Redirect landing page to login route

--- routes/web.php
+++ routes/web.php
@@ -5,7 +5,7 @@ use App\Http\Controllers\BikeController;
 
 // Show the landing page
 Route::get('/', function () {
-    return view('welcome');
+    return redirect()->route('login');
 });
 
 // Login and Register views are handled by Fortify (see FortifyServiceProvider)
