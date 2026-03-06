d779fbc5dd5c164e844c1666f03dd19b8a4c1175
Lee-folkes <154321496+Lee-folkes@users.noreply.github.com>
2026-03-06 15:44:07 +0000

    Remove integrity attributes from Leaflet CSS and JS links in dashboard view

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,10 +1,10 @@
 @extends('layouts.app')
 @push('styles')
     @vite('resources/css/pages/dashboard.css')
-    <link rel="stylesheet" href="https://unpkg.com/leaflet@1.9.4/dist/leaflet.css" integrity="sha256-p4NxAoJBhIIN+hmNHrzRCf9tD/miZyoHS5obTRR9BMY=" crossorigin="" />
+    <link rel="stylesheet" href="https://unpkg.com/leaflet@1.9.4/dist/leaflet.css" crossorigin="" />
 @endpush
 @push ('scripts')
-    <script src="https://unpkg.com/leaflet@1.9.4/dist/leaflet.js" integrity="sha256-20nQCchB9co0qIjJZRGuk2/Z9VM+kNiyxNV1lvTlZBo=" crossorigin=""></script>
+    <script src="https://unpkg.com/leaflet@1.9.4/dist/leaflet.js" crossorigin=""></script>
     @vite('resources/js/pages/dashboard.js')
 @endpush
 @section('content')
