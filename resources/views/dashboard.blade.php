669e68878df10599ce7f32910d1a29bf29533ce8
Lee-folkes <154321496+Lee-folkes@users.noreply.github.com>
2026-04-21 14:55:12 +0100

    Enhance dashboard page with additional comments for clarity on CSS and JS assets

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,10 +1,14 @@
 @extends('layouts.app')
 @push('styles')
+    <!-- Custom CSS for the dashboard page -->
     @vite('resources/css/pages/dashboard.css')
+    <!-- Leaflet CSS for map display -->
     <link rel="stylesheet" href="https://unpkg.com/leaflet@1.9.4/dist/leaflet.css" crossorigin="" />
 @endpush
 @push ('scripts')
+    <!-- Leaflet JS for map functionality -->
     <script src="https://unpkg.com/leaflet@1.9.4/dist/leaflet.js" crossorigin=""></script>
+    <!-- Custom JS for the dashboard page -->
     @vite('resources/js/pages/dashboard.js')
 @endpush
 @section('content')
